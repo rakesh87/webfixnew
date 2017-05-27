2d8c7f38e466fbb8bd4d929b0093cf8315eb0ed3
krantikr <kranti_kumar@hotmail.com>
2017-05-27 21:53:52 +0530

    first

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,13 @@
+class PageController < ApplicationController
+  def index
+  end
+  
+  def contact
+  end
+
+  def contactemail
+  	# @user = { name:'krantik', email: 'kk@gmail.com', mobile: '912345678'}
+  	# raise @user.inspect
+  	# ExampleMailer.sample_email.deliver
+  end
+end
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,6 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'theweinja@gmail.com'
+  layout 'mailer'
+  
+end
+
