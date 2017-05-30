693a96850e4869aee91d6278112ce36e4e554697
krantikr <kranti_kumar@hotmail.com>
2017-05-30 08:50:54 +0530

    config

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Webfix
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'weinja.com' }
+  config.action_mailer.default_url_options = { :host => 'weinja.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
     # SMTP settings for gmail
     config.action_mailer.smtp_settings = {
